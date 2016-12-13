fd0049186e914ee7dd7df40d7ab4d9578efeb750
Samantha Swartz <0501184r@students.wits.ac.za>
2016-12-13 16:09:04 +0200

    Evaluation

---
+++ src/za/co/chss/client/entities/Area.java
@@ -0,0 +1,16 @@
+package za.co.chss.client.entities;
+
+public class Area {
+	
+	private String areaCode;
+	
+	public Area(String areaCode)
+	{
+		this.areaCode = areaCode;
+	}
+
+	public String getAreaCode() {
+		return areaCode;
+	}
+
+}
---
+++ src/za/co/chss/client/lib/PatientSearch.java
@@ -0,0 +1,23 @@
+package za.co.chss.client.lib;
+
+import java.util.List;
+
+import za.co.chss.client.entities.Area;
+import za.co.chss.client.entities.Patient;
+import za.co.chss.client.entities.Vitals;
+
+public class PatientSearch {
+	
+	private List<Patient> listOfPatientsArea = null;
+	
+	public List<Patient> getListOfPatients(Area area)
+	{
+		Vitals v1 = new Vitals("10", "10", "10", "10", "10", "10", "10");
+		Patient p1 = new Patient("Patient1","P1Surname" , "1234567890123", "Male", 31 , "1985/01/01 ", "0812345678", "add1", "add2", v1);
+		Patient p2 = new Patient("Patient2","P2Surname" , "1234567890123", "Female", 34 , "1982/02/02 ", "0812345678", "add1", "add3", v1);
+		listOfPatientsArea.add(p1);
+		listOfPatientsArea.add(p2);
+		return listOfPatientsArea;
+	}
+
+}
