71ceced3de2317d441546341f050cc70e712490d
Samantha Swartz <0501184r@students.wits.ac.za>
2016-12-13 11:31:18 +0200

    Login

---
+++ src/za/co/chss/client/lib/Login.java
@@ -0,0 +1,12 @@
+package za.co.chss.client.lib;
+
+import za.co.chss.client.entities.User;
+
+public class Login {
+
+	public boolean login(User user)
+	{
+		//call api and see if user is valid, if so return true
+		return true;
+	}
+}
