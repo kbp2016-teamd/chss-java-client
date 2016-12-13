7c76d605ea2e3745c3233ca8ba425cfb9ec1c2ee
Samantha Swartz <0501184r@students.wits.ac.za>
2016-12-13 16:21:30 +0200

    Patient creation.

---
+++ src/za/co/chss/client/lib/PatientCreation.java
@@ -0,0 +1,15 @@
+package za.co.chss.client.lib;
+
+import za.co.chss.client.entities.Patient;
+
+public class PatientCreation {
+	
+	private Patient patient = null;
+	
+	public boolean createPatient(Patient patient)
+	{
+		
+		return true;
+	}
+
+}
